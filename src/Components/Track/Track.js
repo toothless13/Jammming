6ab79ef68a12261f1ca57c8a879dc0d046383062
toothless13 <50870607+toothless13@users.noreply.github.com>
2023-02-16 14:53:27 +0000

    Add static components

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Track.css';
+
+export class Track extends React.Component {
+    renderAction() {
+        return (
+            <button className="Track-action">{this.props.isRemoval ? "-" : "+"}</button>
+        );
+    }
+    
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>track name</h3>
+                    <p>track artist | track album</p>
+                </div>
+                {this.renderAction()}
+            </div>
+        );
+    }
+}
