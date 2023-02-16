ebcc34d40e5d729fe53464edcc74dc0fdbe30800
toothless13 <50870607+toothless13@users.noreply.github.com>
2023-02-16 17:12:17 +0000

    Implement 'add to playlist' feature

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,22 +5,34 @@ import {SearchResults} from '../SearchResults/SearchResults';
 import {Playlist} from '../Playlist/Playlist';
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    searchResults : [
-      { name: 'name1', artist: 'artist1', album: 'album1', id: 'id1'},
-      { name: 'name2', artist: 'artist2', album: 'album2', id: 'id2'},
-      { name: 'name3', artist: 'artist3', album: 'album3', id: 'id3'}
-    ],
-    playlistName: 'playlistName',
-    playlistTracks: [
-      { name: 'name1', artist: 'artist1', album: 'album1', id: 'id1'},
-      { name: 'name2', artist: 'artist2', album: 'album2', id: 'id2'},
-      { name: 'name3', artist: 'artist3', album: 'album3', id: 'id3'}
-    ]
-  };
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults : [
+        { name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+        { name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+        { name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+      ],
+      playlistName: 'playlistName',
+      playlistTracks: [
+        { name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4},
+        { name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5},
+        { name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
   render() {
     return (
       <div>
@@ -28,7 +40,8 @@ constructor(props) {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
+            <SearchResults searchResults={this.state.searchResults}
+                            onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
