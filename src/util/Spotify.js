953c65f2f7866d93a24fd7ef417c36a437f371ec
toothless13 <50870607+toothless13@users.noreply.github.com>
2023-02-17 15:43:45 +0000

    Add spotify search request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -27,7 +27,25 @@ const Spotify = {
     },
 
     search(term) {
-
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        });
     }
 }
 
